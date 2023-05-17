c94ae2da07d9f5361c2b85f6323fcbf5c6fa20ef
RayhaanK <rayhaankhan331@gmail.com>
2023-05-17 09:22:32 +0200

    added answers

---
+++ script/code.js
@@ -0,0 +1,20 @@
+// Question 1
+let x = "Hello";
+console.log(typeof(x));
+
+// Question 2
+x = 34; 
+console.log(typeof(x));
+
+// Question Three
+let factual = true;
+console.log(typeof(factual));
+
+let middleName;
+console.log(typeof(middleName));
+
+let colourSelector = null ;
+console.log(typeof(colourSelector));
+
+let symbol = Symbol()
+console.log(typeof(symbol));
